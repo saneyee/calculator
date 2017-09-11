cfc81e92470b6711f258a8a1d4763cd78417dd8a
Gyles Batara and Saneyee Sarkar <gyles@>
2017-09-11 10:46:37 -0700

    Made calculator

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = mult(number1, number2);
+alert(result);
