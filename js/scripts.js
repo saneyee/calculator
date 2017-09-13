0d00bca6735ff0ad89669a98ce34c30da558800d
Saneyee Sarkar and Victor Puentes <victor@>
2017-09-13 14:19:39 -0700

    calculator app with sub multiply and divide options

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,42 @@ var divide = function(number1, number2) {
 
 //USER LOGIC
 $(document).ready(function() {
+
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#outputAdd").text(result);
+
   });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#outputSub").text(result);
+
+  });
+
+  $("form#divi").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#outputDivide").text(result);
+
+  });
+
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#outputMultiply").text(result);
+
+  });
+
+
+
 });
