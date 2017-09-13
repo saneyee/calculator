c19513579b969bb266ea9fcae7a9efccaf0179e0
Saneyee Sarkar and Victor Puentes <saneyee@>
2017-09-13 13:22:37 -0700

    add output element to index.html file so that the answer shows in a box.

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,11 @@ var divide = function(number1, number2) {
 
 //USER LOGIC
 $(document).ready(function() {
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = mult(number1, number2
-  
-alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
