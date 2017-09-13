e9aaa75dbbb84aee0b5b4d581c82162f9c5ca6a8
Saneyee Sarkar and Victor Puentes <victor@>
2017-09-13 11:41:00 -0700

    add changes to index.html file, use form-group class to establish form of the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-var mult = function(number1, number2) {
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+//USER LOGIC
+$(document).ready(function() {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = mult(number1, number2);
-alert(result);
+var result = mult(number1, number2
+  
+alert(add(number1, number2));
+});
